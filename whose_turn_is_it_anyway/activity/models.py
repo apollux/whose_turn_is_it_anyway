42d70eae729d6e103685fd67e9c8dd70036e54c7
Andre Kwakernaak <andre.kwakernaak@gmail.com>
2015-09-27 22:41:04 +0200

    Activities now have occurrences

--- whose_turn_is_it_anyway/activity/models.py
+++ whose_turn_is_it_anyway/activity/models.py
@@ -23,9 +23,20 @@ class Activity(SurrogatePK, Model):
 
 class Participant(SurrogatePK, Model):
     __tablename__ = 'participants'
-    nick_name = Column(db.String(80), unique=True, nullable=False)
+    nick_name = Column(db.String(80), unique=False, nullable=False)
     activity_id = Column(db.Integer, db.ForeignKey('activities.id'))
+    occurrences = relationship('Occurrence')
 
-    def __init__(self, name, **kwargs):
-        db.Model.__init__(self, name=name, **kwargs)
+    def __init__(self, name, activity_id, **kwargs):
+        db.Model.__init__(self, nick_name=name, activity_id=activity_id, **kwargs)
 
+
+class Occurrence(SurrogatePK, Model):
+    # TODO track user whom create occurrence
+    # TODO perhaps needs to be mapped to Activity
+    __tablename__ = 'occurrences'
+    date_time = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
+    participant_id = Column(db.Integer, db.ForeignKey('participants.id'))
+
+    def __init__(self, participant_id, **kwargs):
+        db.Model.__init__(self, participant_id=participant_id, **kwargs)
--- whose_turn_is_it_anyway/activity/views.py
+++ whose_turn_is_it_anyway/activity/views.py
@@ -7,7 +7,7 @@ from flask_login import login_required, current_user
 from whose_turn_is_it_anyway.utils import flash_errors
 
 from .forms import CreateActivityForm
-from .models import Activity
+from .models import Activity, Participant, Occurrence
 
 blueprint = Blueprint('activities', __name__, url_prefix='/activity', static_folder="../static")
 
@@ -15,15 +15,41 @@ blueprint = Blueprint('activities', __name__, url_prefix='/activity', static_fol
 @blueprint.route("/", methods=["GET", "POST"])
 @login_required
 def overview():
-    flash("Woei!", 'success')
-
+    activities = Activity.query.filter_by(creator_id=current_user.get_id()).all()
     form = CreateActivityForm(request.form)
-    form.participants.append_entry("tessthsthstt")
     if form.validate_on_submit():
         new_activity = Activity.create(name=form.name.data, creator_id=current_user.get_id())
+        for participant in form.participants:
+            sanitized_participant = participant.data.strip()
+            if sanitized_participant:
+                Participant.create(name=sanitized_participant, activity_id=new_activity.id)
+
         flash("Activity created, You can now start tracking whose turn it is! {}".format(form.participants.data), 'success')
         return redirect(url_for('activities.overview'))
     else:
         flash_errors(form)
-    return render_template("activity/overview.html", form=form)
+
+    if len(form.participants) == 0:
+        # Make sure to have two fields in the field list when rendering the page
+        form.participants.append_entry("")
+        form.participants.append_entry("")
+    return render_template("activity/overview.html", form=form, activities=activities)
+
+
+@blueprint.route("/activity/<activity_id>", methods=["GET", "POST"])
+@login_required
+def activity_detail(activity_id):
+    activity = Activity.query.filter_by(id=activity_id).one()
+
+    if int(activity.creator_id) != int(current_user.get_id()):
+        flash("Not allowed to access activity", 'warning')
+        return redirect(url_for('activities.overview'))
+
+    if request.method == 'POST':
+        # TODO validate id
+
+        participant_id = next(request.form.keys())
+        Occurrence.create(participant_id=participant_id)
+        return redirect(url_for('activities.activity_detail', activity_id=activity_id))
+    return render_template("activity/activity.html", activity=activity)
 
