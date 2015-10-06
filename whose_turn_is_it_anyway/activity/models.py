31c8e9f9eb601b60f2572e7ecacaec01d1af700a
Andre Kwakernaak <andre.kwakernaak@gmail.com>
2015-10-06 19:01:36 +0200

    Participant has count of occurences instead of list

--- whose_turn_is_it_anyway/activity/models.py
+++ whose_turn_is_it_anyway/activity/models.py
@@ -1,4 +1,6 @@
 import datetime as dt
+from sqlalchemy.orm import column_property
+from sqlalchemy import select, func
 
 from whose_turn_is_it_anyway.database import (
     Column,
@@ -22,24 +24,6 @@ class Activity(SurrogatePK, Model):
         db.Model.__init__(self, name=name, **kwargs)
 
 
-class Participant(SurrogatePK, Model):
-    __tablename__ = 'participants'
-    nick_name = Column(db.String(80), unique=False, nullable=True)
-    activity_id = Column(db.Integer, db.ForeignKey('activities.id'))
-    occurrences = relationship('Occurrence')
-    user_id = Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
-    user = relationship("User", uselist=False)
-
-    def __init__(self, activity_id, name=None, user_id=None, **kwargs):
-        db.Model.__init__(self, nick_name=name, activity_id=activity_id, user_id=user_id, **kwargs)
-
-    def get_name(self):
-        if self.user:
-            return self.user.username
-        else:
-            return self.nick_name
-
-
 class Occurrence(SurrogatePK, Model):
     # TODO track user whom create occurrence
     # TODO perhaps needs to be mapped to Activity
@@ -53,3 +37,28 @@ class Occurrence(SurrogatePK, Model):
 
     def __init__(self, activity_id, participant_id, creator_id, **kwargs):
         db.Model.__init__(self, activity_id=activity_id, participant_id=participant_id, creator_id=creator_id, **kwargs)
+
+
+class Participant(Model):
+    __tablename__ = 'participants'
+    id = db.Column(db.Integer, primary_key=True)
+    nick_name = Column(db.String(80), unique=False, nullable=True)
+    activity_id = Column(db.Integer, db.ForeignKey('activities.id'))
+
+    number_of_occurrences = column_property(select([func.count(Occurrence.id)]).
+                                            where(Occurrence.participant_id == id))
+
+    user_id = Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    user = relationship("User", uselist=False)
+
+    def __init__(self, activity_id, name=None, user_id=None, **kwargs):
+        db.Model.__init__(self, nick_name=name, activity_id=activity_id, user_id=user_id, **kwargs)
+
+    def get_name(self):
+        if self.user:
+            return self.user.username
+        else:
+            return self.nick_name
+
+
+
