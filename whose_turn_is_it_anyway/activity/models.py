d7e0bb0d54b55d52a6079a98fa4e793ae45e086f
Andre Kwakernaak <andre.kwakernaak@gmail.com>
2015-10-07 20:45:26 +0200

    Determine whose turn it is

--- whose_turn_is_it_anyway/activity/models.py
+++ whose_turn_is_it_anyway/activity/models.py
@@ -1,12 +1,12 @@
 import datetime as dt
 from sqlalchemy.orm import column_property
 from sqlalchemy import select, func
+from operator import attrgetter, methodcaller
 
 from whose_turn_is_it_anyway.database import (
     Column,
     db,
     Model,
-    ReferenceCol,
     relationship,
     SurrogatePK,
 )
@@ -23,10 +23,17 @@ class Activity(SurrogatePK, Model):
     def __init__(self, name, **kwargs):
         db.Model.__init__(self, name=name, **kwargs)
 
+    def whose_turn_is_it(self):
+        assert self.participants
+        candidates = sorted(self.participants, key=attrgetter('number_of_occurrences'), reverse=True)
+        candidates = [p for p in candidates if p.number_of_occurrences == candidates[0].number_of_occurrences]
+        candidates = sorted(candidates, key=attrgetter('last_occurrence'))
+        candidates = [p for p in candidates if p.last_occurrence == candidates[0].last_occurrence]
+        candidates = sorted(candidates, key=methodcaller('get_name'))
+        return candidates[0].get_name()
+
 
 class Occurrence(SurrogatePK, Model):
-    # TODO track user whom create occurrence
-    # TODO perhaps needs to be mapped to Activity
     __tablename__ = 'occurrences'
     activity_id = Column(db.Integer, db.ForeignKey('activities.id'))
     date_time = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
@@ -47,6 +54,10 @@ class Participant(Model):
 
     number_of_occurrences = column_property(select([func.count(Occurrence.id)]).
                                             where(Occurrence.participant_id == id))
+
+    last_occurrence = column_property(select([func.count(Occurrence.id)]).
+                                      where(Occurrence.participant_id == id).
+                                      order_by(Occurrence.date_time).limit(1))
 
     user_id = Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
     user = relationship("User", uselist=False)
